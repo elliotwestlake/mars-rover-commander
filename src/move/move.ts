3ebc5ccf4d8da02a751b247377702bf6c6c6eb91
Elliot Westlake <17042376+elliotwestlake@users.noreply.github.com>
2024-05-25 20:25:36 +0100

    chore: simplify move

--- src/move/move.ts
+++ src/move/move.ts
@@ -9,28 +9,24 @@ export const move = ({
   let newPosition = position;
   switch (direction) {
     case Direction.E:
-      if (newPosition.x + 1 > plateauSize.x) {
-        break;
+      if (newPosition.x + 1 <= plateauSize.x) {
+        newPosition.x = newPosition.x + 1;
       }
-      newPosition.x = newPosition.x + 1;
       break;
     case Direction.W:
-      if (newPosition.x - 1 < 0) {
-        break;
+      if (newPosition.x - 1 >= 0) {
+        newPosition.x = newPosition.x - 1;
       }
-      newPosition.x = newPosition.x - 1;
       break;
     case Direction.S:
-      if (newPosition.y - 1 < 0) {
-        break;
+      if (newPosition.y - 1 >= 0) {
+        newPosition.y = newPosition.y - 1;
       }
-      newPosition.y = newPosition.y - 1;
       break;
     case Direction.N:
-      if (newPosition.y + 1 > plateauSize.y) {
-        break;
+      if (newPosition.y + 1 <= plateauSize.y) {
+        newPosition.y = newPosition.y + 1;
       }
-      newPosition.y = newPosition.y + 1;
       break;
   }
 
